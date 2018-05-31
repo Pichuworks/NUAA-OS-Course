ffbc4fbc9205540bba2073a5adcba36ae8820444
橘猫昊昊 <pichuworks@foxmail.com>
2018-05-31 23:21:03 +0800

    sh1.c

--- Vol.2/sh1.c
+++ Vol.2/sh1.c
@@ -1,93 +1,57 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>  
+#include <stdlib.h>
 #include <unistd.h>  
+#include <string.h>  
 #include <errno.h>
 #include <fcntl.h>
-#include "sys/types.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 
-#define EXEC  1
-#define REDIR 2
-#define PIPE  3
-#define LIST  4
-#define BACK  5
+#define LENGTH 255
+#define ARRAYX 255
+#define ARRAYY 10
 
-#define MAXARGS 10
+void analyCMD(char src[], char dest[ARRAYX][ARRAYY]) {
+	char *p;
+	p = strsep(&src, " ");
+	
+	int i = 0;
+	for(i=0; p[i] != '\0'; i++) {
+		dest[0][i] = p[i];
+	}
+	dest[0][i] = '\0';
+	
+	int j = 1;
+	while(p) {
+		p = strsep(&src, " ");
+		if(p) {
+			for(i=0; p[i]!='\0'; i++) {
+				dest[j][i] = p[i];
+			}
+			dest[j][i] = '\0';
+			j++;
+		}
+	}
+	
+	return;
+}
 
-struct command {
-  int type;
-};
-
-struct execCMD {
-  int type;
-  char *argv[MAXARGS];
-  char *eargv[MAXARGS];
-};
-
-int getCMD(char *buf, int nbuf);
-int analysisCMD(char *buf, int nbuf);
+int execCMD(char cmd[ARRAYX][ARRAYY]) {
+	// Debug
+	printf("%s", cmd[0]);
+	return -1;
+}
 
 int main(int argc, char** argv) {
-    static char buf[255];
-    int fd;
-    int pid;
-
-    while((fd = open("console", O_RDWR)) >= 0){
-        if(fd >= 3){
-        close(fd);
-        break;
-        }
-    }
-
-    // Read and run input commands.
-    while(getCMD(buf, sizeof(buf)) >= 0){
-        if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
-            // cd命令需要在主进程实现[这是为什么呢，XV6实现是这样的]
-            // 我傻了，cd肯定要在主进程切换目录
-            buf[strlen(buf)-1] = 0;
-            if(chdir(buf+3) < 0)
-                printf(2, "cannot cd %s\n", buf+3);
-            continue;
-        }
-        
-        pid = fork();
-        if(pid == -1) {
-            printf("fork ERR\n");
-            continue;
-        }
-
-        if(pid == 0)
-            analysisCMD(buf, sizeof(buf));
-
-        wait();
-    }
-
-  return 0;
-
-}
-
-int getCMD(char* buf, int nbuf) {
-  printf("$ ");
-  memset(buf, 0, nbuf);
-  gets(buf);
-  if(buf[0] == 0) // EOF
-    return -1;
-  return 0;
-}
-
-int analysisCMD(char* buf, int nbuf) {
-  // Debug
-  printf("%s\n", buf);
-
-  char *ebuf;
-  struct cmd *cmd;
-
-  ebuf = buf + strlen(buf);
-  cmd = parse(&buf, ebuf);
-
-  return 0;
-}
-
-int parse(char** ps, char* es) {
-  
+	while(1) {
+		char command[LENGTH] = {0};
+		char splitArray[ARRAYX][ARRAYY] = {{0}};
+		printf("$ ");
+		gets(command);
+		analyCMD(command, splitArray);
+		int i = 0;
+		if(execCMD(splitArray) == -1) {
+			return 0;
+		}
+	}
 }
