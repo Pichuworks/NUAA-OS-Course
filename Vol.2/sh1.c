8ba595532c2ae3d7f3b6bc2b7282578d784b3363
PichuTheNeko <pichuworks@foxmail.com>
2018-05-31 21:16:33 +0800

    fuck sh

--- Vol.2/sh1.c
+++ Vol.2/sh1.c
@@ -3,9 +3,29 @@
 #include <string.h>  
 #include <unistd.h>  
 #include <errno.h>
+#include <fcntl.h>
 #include "sys/types.h"
 
-int getcmd(char *buf, int nbuf);
+#define EXEC  1
+#define REDIR 2
+#define PIPE  3
+#define LIST  4
+#define BACK  5
+
+#define MAXARGS 10
+
+struct command {
+  int type;
+};
+
+struct execCMD {
+  int type;
+  char *argv[MAXARGS];
+  char *eargv[MAXARGS];
+};
+
+int getCMD(char *buf, int nbuf);
+int analysisCMD(char *buf, int nbuf);
 
 int main(int argc, char** argv) {
     static char buf[255];
@@ -20,10 +40,11 @@ int main(int argc, char** argv) {
     }
 
     // Read and run input commands.
-    while(getcmd(buf, sizeof(buf)) >= 0){
+    while(getCMD(buf, sizeof(buf)) >= 0){
         if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
-            // Chdir must be called by the parent, not the child.
-            buf[strlen(buf)-1] = 0;  // chop \n
+            // cd命令需要在主进程实现[这是为什么呢，XV6实现是这样的]
+            // 我傻了，cd肯定要在主进程切换目录
+            buf[strlen(buf)-1] = 0;
             if(chdir(buf+3) < 0)
                 printf(2, "cannot cd %s\n", buf+3);
             continue;
@@ -36,7 +57,7 @@ int main(int argc, char** argv) {
         }
 
         if(pid == 0)
-            printf("%s\n", buf);
+            analysisCMD(buf, sizeof(buf));
 
         wait();
     }
@@ -45,11 +66,28 @@ int main(int argc, char** argv) {
 
 }
 
-int getcmd(char *buf, int nbuf) {
+int getCMD(char* buf, int nbuf) {
   printf("$ ");
   memset(buf, 0, nbuf);
-  gets(buf, nbuf);
+  gets(buf);
   if(buf[0] == 0) // EOF
     return -1;
   return 0;
+}
+
+int analysisCMD(char* buf, int nbuf) {
+  // Debug
+  printf("%s\n", buf);
+
+  char *ebuf;
+  struct cmd *cmd;
+
+  ebuf = buf + strlen(buf);
+  cmd = parse(&buf, ebuf);
+
+  return 0;
+}
+
+int parse(char** ps, char* es) {
+  
 }
